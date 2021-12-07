e8b42e38eb7fb111dd041a227bcd57f25e4c84ef
Aburo Senpai <Aburo Senpai@EquipodeAburo.technicolor.net>
2021-12-07 20:25:08 +0100

    Practica Finalizada PSM

---
+++ src/practica/psm/Jugadores.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practica.psm;
+
+/**
+ *
+ * @author Aburo Senpai
+ */
+public class Jugadores {
+    
+    int puntos;
+    public Jugadores(){
+        puntos=0;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+    
+}
